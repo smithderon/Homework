129f855fc7fbb00325aa56c3f5deb12250df131e
Deron Smith <smithderon42@yahoo.com>
2019-04-29 18:01:59 -0500

    Yep

---
+++ Monday/Week_3/examples.py
@@ -0,0 +1,10 @@
+"""
+Given an array of elements, remove duplicates
+and return an array of only unique elements.
+"""
+array = ["Banana", "Bonanza", "Banana", "Nani"]
+
+for items in array:
+    new_array = []
+    if items not in new_array:
+        new_array
